4732e29217f7a62a02cabc3dc4bccfa82b4e52d9
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-03-01 17:24:10 +0800

    Update setup.ps1

--- web/setup.ps1
+++ web/setup.ps1
@@ -2,9 +2,8 @@
 conda install conda-forge::numpy -y
 conda install conda-forge::opencv -y
 conda install conda-forge::flask -y
-conda install conda-forge::tensorflow -y
 conda install conda-forge::pillow -y
 conda install conda-forge::tqdm -y
 conda install conda-forge::gin-config -y
 conda install conda-forge::scipy -y
-pip install mediapipe==0.9.0.1
+pip install tensorflow==2.11.0
