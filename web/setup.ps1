efaac12c98f2ff204e435675b27a2ca6e2c01019
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-03-01 17:22:09 +0800

    setup

---
+++ web/setup.ps1
@@ -0,0 +1,10 @@
+# Install the required libraries
+conda install conda-forge::numpy -y
+conda install conda-forge::opencv -y
+conda install conda-forge::flask -y
+conda install conda-forge::tensorflow -y
+conda install conda-forge::pillow -y
+conda install conda-forge::tqdm -y
+conda install conda-forge::gin-config -y
+conda install conda-forge::scipy -y
+pip install mediapipe==0.9.0.1
